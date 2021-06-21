1d4c2616c68c40406372e247226bdee374092e9b
Joby K <79845600+jobyko@users.noreply.github.com>
2021-06-21 22:18:49 +0530

    [Minor] updated python3 module install

--- PyPi.sh
+++ PyPi.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #
+install_module(){
+	python3 -m pip install pybuilder bandit
+}
+
 install_app(){
 
 	echo "\e[1;34m Initiating pip/pybuilder/bandit installation \e[0m"
     python3 --version
 	if [ $? -eq 0 ]; then	
-		pip3 install pybuilder bandit
+		install_module
 
 	else
 		echo "\e[1;34m Installing python3. \e[0m"
@@ -16,7 +20,7 @@ install_app(){
 			echo "\e[1;31m Python3 install failed, Please do a mannual install  \e[0m"
 			exit 1
 		fi 
-		pip3 install pybuilder bandit
+		install_module
 		echo "Installed pybuilder bandit and pip"
 
 	fi 
